4b96b2c8b0f5a8b5b4450953a82940d82ab832c9
Jerome Chery <jerome.chery@nrco.fr>
2018-09-04 18:03:50 +0200

    Better Split

--- src/components/MainWindow.vue
+++ src/components/MainWindow.vue
@@ -2,12 +2,22 @@
   <Window>
     <Toolbar type="header" title="Header">
       <ToolbarActions>
-
+        <ButtonGroup class="pull-left">
+          <Button size="sm" :active="shown.sidebar" @click.native="shown.sidebar = !shown.sidebar">
+            <Icon icon="home"></Icon>
+          </Button>
+          <Button size="sm" :active="shown.mainPane" @click.native="shown.mainPane = !shown.mainPane">
+            <Icon icon="home"></Icon>
+          </Button>
+          <Button size="sm" :active="shown.altPane" @click.native="shown.altPane = !shown.altPane">
+            <Icon icon="home"></Icon>
+          </Button>
+        </ButtonGroup>
       </ToolbarActions>
     </Toolbar>
     <WindowContent>
         <PaneGroup>
-          <Pane size="mini" :sidebar="true" style="display:block">sidebar</Pane>
+          <Pane size="mini" :sidebar="true" :class="{invisible: !shown.sidebar}">sidebar</Pane>
           <Pane>
             <VueSplit
               :elements="panes"
@@ -16,7 +26,7 @@
               :gutter-size="10"
               :snap-offset="50"
             >
-              <div id="mainPane" class="hello">
+              <div id="mainPane" class="hello" :class="{invisible: !shown.mainPane}">
                 <h1>{{ msg }}</h1>
                 <p>
                   For guide and recipes on how to configure / customize this project,<br>
@@ -29,7 +39,7 @@
                   <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
                 </ul>
               </div>
-              <div id="altPane">
+              <div id="altPane" :class="{invisible: !shown.altPane}">
                 <h3>Essential Links</h3>
                 <ul>
                   <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
@@ -56,17 +66,33 @@
 </template>
 
 <script>
-import VueSplit from "vue-splitjs";
-import { Window, WindowContent, PaneGroup, Pane, Toolbar, ToolbarActions } from "vue-photonkit";
+import VueSplit from "./VueSplit.vue";
+import { Window, WindowContent, PaneGroup, Pane, Toolbar, ToolbarActions, ButtonGroup, Button, Icon } from "vue-photonkit";
 export default {
-  name: "HelloWorld",
+  name: "MainWindow",
   props: {
     msg: String
   },
   data: () => ({
-    panes: ["#mainPane", "#altPane"]
+    shown: {
+      sidebar: true,
+      mainPane : true,
+      altPane: true
+    }
   }),
-  components: { VueSplit, Window, WindowContent, PaneGroup, Pane, Toolbar, ToolbarActions }
+  computed: {
+    panes: function() {
+      var res = [];
+      if (this.shown.mainPane) {
+        res.push('#mainPane');
+      }
+      if (this.shown.altPane) {
+        res.push('#altPane');
+      }
+      return res;
+    }
+  },
+  components: { VueSplit, Window, WindowContent, PaneGroup, Pane, Toolbar, ToolbarActions, ButtonGroup, Button, Icon }
 }
 </script>
 
@@ -85,5 +111,8 @@ li {
 }
 a {
   color: #42b983;
+}
+.invisible {
+  display: none;
 }
 </style>
---
+++ src/components/VueSplit.vue
@@ -0,0 +1,160 @@
+<template lang="html">
+  <div class="split-container"
+    :class="{'split-container--horizontal': direction === 'horizontal'}"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/javascript">
+import Split from 'split.js'
+
+export default {
+  name: "VueSplit",
+  data: function() {
+    return {
+      instance: {}
+    }
+  },
+  props: {
+    elements: {
+      // Array of target split element ids
+      type: Array,
+      required: true,
+      validator(value) {
+        // must be array of strings
+        const isValid = value.every(i => typeof i === "string")
+        return isValid
+      }
+    },
+
+    direction: {
+      // Direction to split: horizontal or vertical.
+      type: String,
+      default: 'horizontal',
+      validator(value) {
+        let allowedValues = ['horizontal', 'vertical']
+        const isValid = allowedValues.includes(value)
+        return isValid
+      }
+    },
+
+    sizes: {
+      // Initial sizes of each element in percents.
+      type: Array,
+      default: null,
+      validator(value) {
+        // must be array of numbers
+        const isValid = value.every(i => typeof i === "number")
+        return isValid
+      }
+    },
+
+    minSize: {
+      // Minimum size of each element in pixels.
+      type: [Number, Array],
+      default: 100
+    },
+
+    gutterSize: {
+      // Gutter size in pixels.
+      type: Number,
+      default: 10
+    },
+
+    snapOffset: {
+      // Snap to minimum size offset.
+      type: Number,
+      default: 30
+    },
+
+    cursor: {
+      // Cursor to display while dragging.
+      type: String,
+      default: 'grabbing'
+    }
+  },
+
+  mounted () {
+    this.instance = Split(this.elements, {
+      direction: this.direction,
+      sizes: this.sizes,
+      minSize: this.minSize,
+      gutterSize: this.gutterSize,
+      snapOffset: this.snapOffset,
+      cursor: this.cursor,
+      onDrag: this.onDrag,
+      onDragStart: this.onDragStart,
+      onDragEnd: this.onDragEnd
+    })
+  },
+
+  updated () {
+    this.instance.destroy();
+    this.instance = Split(this.elements, {
+      direction: this.direction,
+      sizes: this.sizes,
+      minSize: this.minSize,
+      gutterSize: this.gutterSize,
+      snapOffset: this.snapOffset,
+      cursor: this.cursor,
+      onDrag: this.onDrag,
+      onDragStart: this.onDragStart,
+      onDragEnd: this.onDragEnd
+    })
+  },
+  methods: {
+    onDrag() {
+      this.$emit('onDrag')
+    },
+
+    onDragEnd() {
+      this.$emit('onDragEnd')
+    },
+
+    onDragStart() {
+      this.$emit('onDragStart')
+    }
+  }
+}
+</script>
+
+<style>
+.split-container {
+  height: 100%;
+}
+
+.split-container > * {
+  position: relative;
+  overflow-y: hidden;
+}
+
+.split-container--horizontal {
+  display: flex;
+}
+
+.gutter {
+  background-color: #eee;
+  background-repeat: no-repeat;
+  background-position: 50%;
+}
+
+.gutter.gutter-vertical {
+  background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAFAQMAAABo7865AAAABlBMVEVHcEzMzMzyAv2sAAAAAXRSTlMAQObYZgAAABBJREFUeF5jOAMEEAIEEFwAn3kMwcB6I2AAAAAASUVORK5CYII=');
+  cursor: ns-resize;
+}
+
+.gutter.gutter-horizontal {
+  float: left;
+  cursor: ew-resize;
+  background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAeCAYAAADkftS9AAAAIklEQVQoU2M4c+bMfxAGAgYYmwGrIIiDjrELjpo5aiZeMwF+yNnOs5KSvgAAAABJRU5ErkJggg==')
+}
+
+.split {
+  -webkit-box-sizing: border-box;
+     -moz-box-sizing: border-box;
+          box-sizing: border-box;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+</style>
